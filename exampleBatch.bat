c08efe80799ad47c8044c49386911c5f5c8688e9
Rose <blr76@txstate.edu>
2018-03-22 16:20:40 -0500

    Batch example

---
+++ exampleBatch.bat
@@ -0,0 +1,8 @@
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\2_Baird_Lake\
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\5_Eunice_SE\
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\6_Figure_Seven_Ranch\
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\7_Five_Wells_Ranch\
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\8_Five_Wells_Ranch_NW\
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\9_Five_Wells_Ranch_SE\
+C:\Python27\ArcGIS10.4\python.exe C:\Scripts\quadMergeNDVI.py I:\1_ANDREWS\10_Five_Wells_SW\
+PAUSE
